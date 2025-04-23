f134d26703859135aa41117f9b8b50849357e993
Alfaiz-embold <103499815+Alfaiz-embold@users.noreply.github.com>
2025-04-24 02:50:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -29,4 +29,32 @@ System.out.println("Hello");
 	  System.out.println("Hello");
 	  System.out.println("H");
   }
+// public printSTMT(){
+//     System.out.println("Hello");
+// 	System.out.println("Hello");
+// 	System.out.println("Hello");
+// 	  System.out.println("Hello");
+// 	  System.out.println("H");
+// 	   System.out.println("Hello");
+// 	System.out.println("Hello");
+// 	System.out.println("Hello");
+// 	  System.out.println("Hello");
+// 	  System.out.println("H");
+// System.out.println("Hello");
+// 	  System.out.println("Hello");
+// 	  System.out.println("H");
+// 	logger.info("Hello");
+// 	logger.info("Hello");
+// 	logger.info("Hello");
+// 	logger.info("Hello");
+// 	logger.info("Hello");
+// 	logger.info("Hello");
+// 	logger.info("Hello");
+// 	//  if(true){}
+// 	//  try{}catch(Exception e){}
+// 	  //new code added on 11-Jul-2024
+// 	  System.out.println("Hello");
+// 	  System.out.println("Hello");
+// 	  System.out.println("H");
+//   }
 }
